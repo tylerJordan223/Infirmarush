292194125fd3bc700a3ec676fd35d2f7c77e3d63
tylerJordan223 <tjlinkmaster22@gmail.com>
2024-11-02 00:10:06 -0400

    patient timer works

--- Assets/Scripts/Other People/BedScript.cs
+++ Assets/Scripts/Other People/BedScript.cs
@@ -5,9 +5,30 @@ using UnityEngine;
 public class BedScript : MonoBehaviour
 {
     public bool has_patient;
+    public Transform my_bar;
+    public float patient_time_limit;
+    private float time_spent;
 
     private void Start()
     {
         has_patient = false;
+        my_bar = this.transform.GetChild(0);
+        my_bar.localScale = new Vector3(0f, my_bar.localScale.y, my_bar.localScale.z);
+        patient_time_limit = 0f;
+        time_spent = 0f;
+    }
+
+    private void Update()
+    {
+        if(time_spent < patient_time_limit)
+        {
+            my_bar.localScale = new Vector3((-1+(time_spent/patient_time_limit)),my_bar.localScale.y,0f);
+            time_spent += Time.deltaTime;
+        }
+        else
+        {
+            patient_time_limit = 0f;
+            time_spent = 0f;
+        }
     }
 }
--- Assets/Scripts/Other People/PatientScript.cs
+++ Assets/Scripts/Other People/PatientScript.cs
@@ -7,11 +7,13 @@ public class PatientScript : MonoBehaviour
 {
     private Transform t;
     private bool settled;
+    public float time_limit;
 
     private void Start()
     {
         t = GetComponent<Transform>();
         settled = false;
+        time_limit = UnityEngine.Random.Range(10f, 20f);
     }
 
     public IEnumerator GoToBed(GameObject bed)
@@ -21,7 +23,6 @@ public class PatientScript : MonoBehaviour
         //makes sure the guy is close enough
         while (Vector2.Distance(t.position, bed.transform.position) > 0.1f)
         {
-            Debug.Log(Vector2.Distance(t.position, bed.transform.position));
             //move towards bed
             t.position = Vector2.Lerp(t.position, bed.transform.position, Time.deltaTime * 3f);
             //rotate like crazy
@@ -29,11 +30,10 @@ public class PatientScript : MonoBehaviour
             yield return new WaitForEndOfFrame();
         }
 
-        Debug.Log("Made it out!");
-
         t.SetParent(bed.transform, true);
         t.rotation = Quaternion.Euler(0f, 0f, 0f);
         t.position = bed.transform.position;
+        bed.GetComponent<BedScript>().patient_time_limit = time_limit;
         settled = true;
         yield return null;
     }
